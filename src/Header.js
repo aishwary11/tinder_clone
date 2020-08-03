a76ee08c3accb74d90f1d5699c53a724de7a7e7d
aishwary11 <aishwary46@gmail.com>
2020-08-03 16:53:03 +0530

    added

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+const Header = () => {
+  return (
+    <div>
+      <PersonIcon />
+      <h1>Header</h1>
+      <ForumIcon />
+    </div>
+  )
+}
+
+export default Header
